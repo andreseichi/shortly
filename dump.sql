0fe6234afc54d0e3ff269fed1a62be4b29353c9f
andreseichi <andre_seichi@hotmail.com>
2022-08-03 00:08:03 -0300

    constraint default na sessions

--- dump.sql
+++ dump.sql
@@ -12,5 +12,5 @@ CREATE TABLE sessions(
   id SERIAL PRIMARY KEY,
   user_id INTEGER NOT NULL,
   token TEXT NOT NULL,
-  created_at TIMESTAMP NOT NULL
+  created_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
