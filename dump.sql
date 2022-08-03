d857751a0d07ae630ec606d331a24d65714679e8
andreseichi <andre_seichi@hotmail.com>
2022-08-02 23:16:35 -0300

    dump sql

---
+++ dump.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE "shortly";
+CREATE TABLE users(
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(120) NOT NULL,
+  email TEXT NOT NULL,
+  password TEXT NOT NULL
+);
+INSERT INTO users(name, email, password)
+VALUES('test', 'teste@teste.com', 'password'),
+  ('test2', 'teste2@teste.com', 'password2');
+CREATE TABLE sessions(
+  id SERIAL PRIMARY KEY,
+  user_id INTEGER NOT NULL,
+  token TEXT NOT NULL,
+  created_at TIMESTAMP NOT NULL
+);
