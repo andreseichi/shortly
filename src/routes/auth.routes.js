e676727a571dbbc683c2ffb538153546e2ad9747
andreseichi <andre_seichi@hotmail.com>
2022-08-03 00:07:08 -0300

    rota signin

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 
-import { createUser } from '../controllers/authController.js';
+import { createUser, signIn } from '../controllers/authController.js';
 import { validateCreateUser } from '../middlewares/validateCreateUser.js';
+import { validateLogin } from '../middlewares/validateLogin.js';
 
 export const authRouter = Router();
 
 authRouter.post('/signup', validateCreateUser, createUser);
+authRouter.post('/signin', validateLogin, signIn);
