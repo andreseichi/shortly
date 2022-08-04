2ba3f6b7b913f55dfdcf54a3740df28e349df6e4
andreseichi <andre_seichi@hotmail.com>
2022-08-03 22:26:00 -0300

    get url by id

--- src/controllers/urlsController.js
+++ src/controllers/urlsController.js
@@ -30,3 +30,23 @@ export async function shortenUrl(req, res) {
     return res.status(500).send(error);
   }
 }
+
+export async function getUrlById(req, res) {
+  const { id } = req.params;
+
+  try {
+    const { rows: url } = await connection.query(
+      'SELECT id, short_url_link AS "shortUrl", url_link AS url FROM urls WHERE id = $1',
+      [id]
+    );
+
+    if (url.length === 0) {
+      return res.status(404).send('Url not found');
+    }
+
+    return res.status(200).send(url[0]);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send(error);
+  }
+}
--- src/routes/urls.routes.js
+++ src/routes/urls.routes.js
@@ -1,8 +1,9 @@
 import { Router } from 'express';
 
 import { authentication } from '../middlewares/authentication.js';
-import { shortenUrl } from '../controllers/urlsController.js';
+import { getUrlById, shortenUrl } from '../controllers/urlsController.js';
 
 export const urlsRouter = Router();
 
 urlsRouter.post('/urls/shorten', authentication, shortenUrl);
+urlsRouter.get('/urls/:id', getUrlById);
