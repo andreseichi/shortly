ae357ec92867ddf116a614694497e2ec5cadc23c
andreseichi <andre_seichi@hotmail.com>
2022-08-05 23:43:03 -0300

    dump db e tabelas

--- dump.sql
+++ dump.sql
@@ -3,11 +3,9 @@ CREATE TABLE users(
   id SERIAL PRIMARY KEY,
   name VARCHAR(120) NOT NULL,
   email TEXT NOT NULL,
-  password TEXT NOT NULL
+  password TEXT NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
-INSERT INTO users(name, email, password)
-VALUES('test', 'teste@teste.com', 'password'),
-  ('test2', 'teste2@teste.com', 'password2');
 CREATE TABLE sessions(
   id SERIAL PRIMARY KEY,
   user_id INTEGER NOT NULL REFERENCES "users"("id"),
